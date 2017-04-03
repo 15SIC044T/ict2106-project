aaebb04c227f06a8f29e816956a150712c2ab852
Arifah <Nurul.Arifah_2015@SIT.SingaporeTech.edu.sg>
2017-04-03 17:21:08 +0800

    UI and homepage fix

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookStore.DAL;
+using BookStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +12,22 @@ namespace BookStore.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            using (BookStoreContext db = new BookStoreContext())
+            {
+                
+
+                var comps = (from c in db.Items
+                             join r in db.Reviews
+                             on c.itemID.ToString() equals r.itemID
+                             select new RecommendedList
+                                 {
+                                     itemID = r.itemID,
+                                     iImage = c.iImage,
+                                     iName = c.iName
+                                 }).Distinct().ToList();
+
+                return View(comps);
+            }
         }
 
         public ActionResult About()
---
+++ BookStore/BookStore/Models/RecommendedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class RecommendedList
+    {
+        public Item Item { get; set; }
+        public string iImage { get; set; }
+        public string iName { get; set; }
+        public IEnumerable<Item> Items { get; set; }
+
+        public Review Review { get; set; }
+        public string itemID { get; set; }
+        public IEnumerable<Review> Reviews { get; set; }
+    }
+}
